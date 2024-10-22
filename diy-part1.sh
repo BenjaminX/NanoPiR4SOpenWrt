90f3ea3f72d1d3afe3285f8cc60d6bc89a49fedc
Benjamin <benjaminx@gmail.com>
2024-10-22 17:39:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,3 +24,6 @@ echo '### Argon Theme Config ###'
 
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
 echo '### Shutdown Router ###'
+
+git clone https://github.com/BenjaminX/luci-app-syncdial.git package/lean/luci-app-syncdial
+echo '### luci-app-syncdial ###'
