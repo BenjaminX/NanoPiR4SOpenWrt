b3d9324cb13f90eb6ad15204c60db0602f7bbf06
Benjamin <benjaminx@gmail.com>
2025-02-07 17:06:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -31,7 +31,7 @@ echo '### luci-app-syncdial ###'
 
 # 移除 openwrt feeds 自带的核心包
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-# git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
+git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
 
 # 更新 golang 1.23 版本
 rm -rf feeds/packages/lang/golang
